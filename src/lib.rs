48d80963e64f9ff6602a3bba0d64ad3f6e448c81
Liam Kinne <liam@kinne.io>
2024-04-01 15:37:56 +1000

    add unit tests with real data

--- src/lib.rs
+++ src/lib.rs
@@ -56,6 +56,17 @@ impl<'a> Packet<'a> {
 mod tests {
     use super::*;
 
+    /// Taken from HF2 spec.
+    const TEST_PACKET: &[&[u8]] = &[
+        &[0x83, 0x01, 0x02, 0x03, 0xAB, 0xFF, 0xFF, 0xFF],
+        &[0x85, 0x04, 0x05, 0x06, 0x07, 0x08],
+        &[0x80, 0xDE, 0x42, 0x42, 0x42, 0x42, 0xFF, 0xFF],
+        &[
+            0xD0, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15,
+            0x16, 0x17, 0xFF, 0xFF, 0xFF,
+        ],
+    ];
+
     #[test]
     fn test_packet() {
         let packet = Packet(&[0, 0xFF, 0xFF]);
@@ -66,5 +77,34 @@ mod tests {
         assert!(packet.serial_stdout());
         let packet = Packet(&[0xC0, 0xFF, 0xFF]);
         assert!(packet.serial_stderr());
+    }
+
+    #[test]
+    fn test_stdout() {
+        let packet = Packet(TEST_PACKET[0]);
+        assert!(packet.serial_stdout());
+        assert_eq!(packet.len(), 3);
+        assert_eq!(packet.data(), &[0x01, 0x02, 0x03]);
+
+        let packet = Packet(TEST_PACKET[1]);
+        assert!(packet.serial_stdout());
+        assert_eq!(packet.len(), 5);
+        assert_eq!(packet.data(), &[0x04, 0x05, 0x06, 0x07, 0x08]);
+
+        let packet = Packet(TEST_PACKET[2]);
+        assert!(packet.serial_stdout());
+        assert_eq!(packet.len(), 0);
+        assert_eq!(packet.data(), &[]);
+
+        let packet = Packet(TEST_PACKET[3]);
+        assert!(packet.serial_stderr());
+        assert_eq!(packet.len(), 16);
+        assert_eq!(
+            packet.data(),
+            &[
+                0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16,
+                0x17, 0xFF
+            ]
+        );
     }
 }
